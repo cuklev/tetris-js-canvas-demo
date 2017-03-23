2080b33f8d864fe1f669d41e2f483dc6eec306d8
Kristiyan Tsaklev <cuki@cukii.tk>
2017-03-23 18:49:41 +0200

    Draw tetris grid

--- js/main.js
+++ js/main.js
@@ -11,6 +11,10 @@ const requestAnimationFrame = (window.requestAnimationFrame
 canvas.width = 800;
 canvas.height = 600;
 
+const TETRIS_ROWS = 18;
+const TETRIS_COLS = 10;
+const TETRIS_CELL_SIZE = 20;
+
 let x = 10;
 let y = 10;
 
@@ -18,10 +22,24 @@ function update() {
 	x += 1;
 }
 
+function drawGrid() {
+	for(let i = 0; i <= TETRIS_ROWS; i += 1) {
+		context.moveTo(0, TETRIS_CELL_SIZE * i);
+		context.lineTo(TETRIS_COLS * TETRIS_CELL_SIZE, TETRIS_CELL_SIZE * i);
+		context.stroke();
+	}
+
+	for(let i = 0; i <= TETRIS_COLS; i += 1) {
+		context.moveTo(TETRIS_CELL_SIZE * i, 0);
+		context.lineTo(TETRIS_CELL_SIZE * i, TETRIS_ROWS * TETRIS_CELL_SIZE);
+		context.stroke();
+	}
+}
+
 function draw() {
 	context.clearRect(0, 0, canvas.width, canvas.height);
 
-	context.fillRect(x, y, 10, 10);
+	drawGrid();
 
 	requestAnimationFrame(draw);
 }
